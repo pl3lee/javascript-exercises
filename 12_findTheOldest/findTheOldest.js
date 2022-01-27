de67d696ecbaffe50cc2b093a5b2303434a17adc
pl3lee <billylee123@gmail.com>
2022-01-27 15:59:00 -0500

    Finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
+    people.sort(function(a, b) {
+        let aAge = 0;
+        let bAge = 0;
+        if ('yearOfDeath' in a) {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        } else {
+            aAge = currentYear - a.yearOfBirth;
+        }
+        if ('yearOfDeath' in b) {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        } else {
+            bAge = currentYear - b.yearOfBirth;
+        }
+        if (bAge > aAge) {
+            return 1;
+        } else {
+            return -1;
+        }
+    })
+    return people[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
