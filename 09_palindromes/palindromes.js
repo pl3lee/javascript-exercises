d612121d43ee24a6b69fac8ed1f1dc59e4191081
pl3lee <billylee123@gmail.com>
2022-01-27 00:03:41 -0500

    Finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const reverseString = function(str) {
+    let toReturn = "";
+    strlen = str.length;
+    for (let i = 0; i < strlen; i++) {
+        toReturn += str.substr(-1);
+        str = str.slice(0, -1);
+    }
+    return toReturn;
+};
+function removePunctuation(str) {
+    str = str.replace(/[^A-Za-z0-9]/g,"");
+    return str;
+}
+const palindromes = function (str) {
+    let strReversed = removePunctuation(reverseString(str)).toLowerCase();
+    return strReversed === removePunctuation(str).toLowerCase() ? true : false;
 };
 
 // Do not edit below this line
