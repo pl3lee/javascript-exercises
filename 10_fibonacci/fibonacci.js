206a38566fefd8e925e1c556f167b16e2cceae1e
pl3lee <billylee123@gmail.com>
2022-01-27 15:32:27 -0500

    Finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (Number(num) < 1) {
+        return "OOPS"
+    }
+    num = Number(num)
+    let firstNum = 1;
+    let secondNum = 1;
+    for (let i = 3; i <= num; i++) {
+        let tempNum = firstNum;
+        firstNum = secondNum;
+        secondNum = Number(tempNum) + Number(firstNum);
+    }
+    return secondNum;
 };
 
 // Do not edit below this line
