5e2fce8c6dcc74241ef64868f13d13bad516438c
pl3lee <billylee123@gmail.com>
2022-01-26 21:59:55 -0500

    Finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if ((num1 < 0 || num2 < 0) || (typeof num1 !== 'number') || (typeof num2 !== 'number')) {
+        return 'ERROR';
+    }
+    let tempNum = 0
+    let larger = Math.max(num1, num2);
+    let smaller = Math.min(num1, num2);
+    for (let i = smaller; i <= larger; i++) {
+        tempNum += i;
+    }
+    return tempNum;
 };
 
 // Do not edit below this line
