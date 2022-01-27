4360eba5f7793dcf4814eaee2f5ad5830adcca1a
pl3lee <billylee123@gmail.com>
2022-01-27 16:02:47 -0500

    Finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -3,22 +3,11 @@ const findTheOldest = function(people) {
     people.sort(function(a, b) {
         let aAge = 0;
         let bAge = 0;
-        if ('yearOfDeath' in a) {
-            aAge = a.yearOfDeath - a.yearOfBirth;
-        } else {
-            aAge = currentYear - a.yearOfBirth;
-        }
-        if ('yearOfDeath' in b) {
-            bAge = b.yearOfDeath - b.yearOfBirth;
-        } else {
-            bAge = currentYear - b.yearOfBirth;
-        }
-        if (bAge > aAge) {
-            return 1;
-        } else {
-            return -1;
-        }
-    })
+        ('yearOfDeath' in a) ? (aAge = a.yearOfDeath - a.yearOfBirth) :  (aAge = currentYear - a.yearOfBirth);
+        ('yearOfDeath' in b) ? (bAge = b.yearOfDeath - b.yearOfBirth) : (bAge = currentYear - b.yearOfBirth);
+        
+        return ((bAge > aAge) ? 1 : -1);
+    });
     return people[0];
 };
 
