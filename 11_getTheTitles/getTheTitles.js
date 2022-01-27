b7d74a7a78ee52dd2458bb20a42e9536f1de326e
pl3lee <billylee123@gmail.com>
2022-01-27 15:39:50 -0500

    Finished exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.reduce(function(bookTitles, book) {
+        bookTitles.push(book.title);
+        return bookTitles;
+    }, [])
 };
 
 // Do not edit below this line
